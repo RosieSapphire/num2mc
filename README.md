452b80b80e55858f1931e0cb80248dd8dd12ad74
Rosie Sapphire <jayjaygames40@gmail.com>
2022-11-03 05:30:15 -0500

    Added opposite functionality via an additional parameter

--- README.md
+++ README.md
@@ -1,20 +1,29 @@
 # Num2MC
-A very basic command-line tool that converts a desired number of blocks into
-how many stacks you would need, and how much would be left over.
+A very basic command-line tool that converts a desired number of blocks into</br>
+how many stacks you would need, and how much would be left over.</br>
 
-To compile it, just clone the repository with
+To compile it, just clone the repository with</br>
 `git clone https://github.com/RosieSapphire/num2mc.git`
 
-Then just type `make`.
+Then `cd` into the directory and type `make`.
 
-If you want to install it to your local bin directory (make it so you can run
-it anywhere), then type `sudo make install`, and `sudo make uninstall` if you
-decide to remove it.
+If you want to install it to your local bin directory, then type</br>
+`sudo make install` or `sudo make uninstall` if you decide to remove it.
 
-As for running it, just type `num2mc`, followed by the amount of blocks, so for
-example:
+As for running it, just type `num2mc`, followed by the amount of blocks.
+
+So for example:</br>
 `num2mc 420`
 would produce
 `For 420, you need 6 stacks, with 36 left over.`
 
+If you want the opposite functionality, where you pass in the amount of stacks</br>
+and remainder you want, it will give you how many total blocks that would be.
+
+So instead if you did</br>
+`num2mc 6 36`
+you'd get</br>
+`For 6 stacks and 36 remaining, you need 420 blocks.'
+
 That's it.
+
--- num2mc.c
+++ num2mc.c
@@ -8,7 +8,8 @@
 #define COLOR_YELLOW "\033[0;33m"
 
 int32_t main(int32_t argc, char **argv) {
-	uint64_t blocks_input;
+	uint64_t input_a;
+	uint64_t input_b = 0;
 
 	if(!argc) {
 		fprintf(stderr, "%sSomething seriously wrong has occured for this "
@@ -17,31 +18,62 @@ int32_t main(int32_t argc, char **argv) {
 	}
 
 	if(argc < 2) {
-		fprintf(stderr, "%sPlease specify number of blocks.%s\n",
-				COLOR_RED, COLOR_DEFAULT);
+		fprintf(stderr, "%sPlease specify number of blocks or number of "
+				"stacks and remainder.%s\n", COLOR_RED, COLOR_DEFAULT);
 		return 1;
 	}
 
-	blocks_input = atoi(argv[1]);
-	if(argc > 2) {
+	input_a = atoi(argv[1]);
+	if(argc > 3) {
 		fprintf(stderr, "%sToo many arguments", COLOR_RED);
-		if(!blocks_input) {
+		if(!input_a || !input_b) {
 			fprintf(stderr, ", and invalid input. Please try again");
 		}
 		fprintf(stderr, ".%s\n", COLOR_DEFAULT);
 		return 1;
 	}
 
-	if(!blocks_input) {
-		fprintf(stderr, "%sInvalid input. Please try again.%s\n",
-				COLOR_RED, COLOR_DEFAULT);
-		return 1;
+	switch(argc) {
+		case 2:
+			if(!input_a) {
+				fprintf(stderr, "%sInvalid input. Please try again.%s\n",
+						COLOR_RED, COLOR_DEFAULT);
+				return 1;
+			}
+
+			printf("%sFor %s%lu%s, you need %s%lu%s stacks, "
+					"with %s%lu%s left over.\n%s",
+					COLOR_YELLOW, COLOR_GREEN, input_a, COLOR_YELLOW,
+					COLOR_GREEN, input_a / 64, COLOR_YELLOW, COLOR_GREEN,
+					input_a % 64, COLOR_YELLOW, COLOR_DEFAULT);
+			break;
+
+		case 3:
+			input_b = atoi(argv[2]);
+			if(!input_a) {
+				fprintf(stderr, "%sInvalid input (1). Please try again.%s\n",
+						COLOR_RED, COLOR_DEFAULT);
+				return 1;
+			}
+
+			if(!input_b && *argv[2] - '0') {
+				fprintf(stderr, "%sInvalid input (2). Please try again.%s\n",
+						COLOR_RED, COLOR_DEFAULT);
+				return 1;
+			}
+
+			printf("%sFor %s%lu%s stacks and %s%lu%s remaining, you need "
+					"%s%lu%s blocks.%s\n",
+					COLOR_YELLOW, COLOR_GREEN, input_a, COLOR_YELLOW,
+					COLOR_GREEN, input_b, COLOR_YELLOW, COLOR_GREEN,
+					input_a * 64 + input_b, COLOR_YELLOW, COLOR_DEFAULT);
+			break;
+
+		default:
+			fprintf(stderr, "%sIllegal state achieved. WTF did you do?\n%s",
+					COLOR_RED, COLOR_DEFAULT);
+			return 1;
 	}
-	printf("%sFor %s%lu%s, you need %s%lu%s stacks, "
-			"with %s%lu%s left over.\n%s",
-			COLOR_YELLOW, COLOR_GREEN, blocks_input, COLOR_YELLOW, COLOR_GREEN,
-			blocks_input / 64, COLOR_YELLOW, COLOR_GREEN, blocks_input % 64,
-			COLOR_YELLOW, COLOR_DEFAULT);
 
 	return 0;
 }
